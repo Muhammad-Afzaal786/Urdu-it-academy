a6638302ea740df4df347983cfb2fef6b3fa54e1
afzaal-9795 <muhammadafzaal022@pmxhq.com>
2023-10-28 00:00:32 +0500

    updated code

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 //  @type {import('next').NextConfig}
 const nextConfig = {
+  // reactStrictMode: true,
+  // distDir: "build",
+  // swcMinify: true,
+  // experimental: {
+  //   appDir: true,
+  // },
+  // images: {
+  //   domains: ["www.urduitacademy.com"],
+  //   unoptimized: true,
+  // },
+
+  // output: "export",
   reactStrictMode: true,
-  distDir: "build",
+  distDir: "out", // Or use the default "out" if it matches your project's configuration.
   swcMinify: true,
   experimental: {
     appDir: true,
@@ -10,29 +22,7 @@ const nextConfig = {
     domains: ["www.urduitacademy.com"],
     unoptimized: true,
   },
-
-  output: "export",
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/api/(.*)",
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-};
+  target: "experimental-serverless-trace",
+verce};
 
 module.exports = nextConfig;
