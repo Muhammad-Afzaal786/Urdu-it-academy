96a28f98baed3d36e4a93d6bdbf17e364b165234
afzaal-9795 <muhammadafzaal022@pmxhq.com>
2023-11-13 20:00:17 +0500

    Ui changes

--- components/home/Blog.jsx
+++ components/home/Blog.jsx
@@ -4,13 +4,13 @@ import React, { useEffect, useState } from "react";
 //import Loader from "@/ui/Loader";
 import { SC } from "@/app/Apicall/ServerCall";
 import { blogData } from "@/app/Apicall/endPoints";
-import { basePathApi } from "../../app/Apicall/basePathApi"
-import BlogPage from "../../app/blogs/cards"
+import { basePathApi } from "../../app/Apicall/basePathApi";
+import BlogPage from "../../app/blogs/cards";
 const Blog = () => {
   const [productData, setProductData] = useState([]);
   //const [loading, setLoading] = useState(true);
   // const [data, setData] = useState(null);
-   const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   // const [error, setError] = useState(null);
 
   // useEffect(() => {
@@ -47,23 +47,19 @@ const Blog = () => {
   //   fetchBlogs();
   // }, []);
   useEffect(() => {
-
     fetchData();
-
   }, []);
 
   const fetchData = () => {
     SC.postCall(blogData).then((res) => {
       if (res) {
         setProductData(res.data.data.data);
-       
-       
       } else {
         console.log("Something went wrong");
       }
     });
   };
-console.log(productData, "Pr")
+  console.log(productData, "Pr");
   // if (isLoading) {
   //   return (
   //     <section className="lg:pt-20 pt-14">
@@ -92,72 +88,69 @@ console.log(productData, "Pr")
       {/* <BlogPage productData={ productData} /> */}
       {/* blog content */}
       <div className="flex md:flex-nowrap flex-wrap  -mx-2 mt-9 lg:mt-12 sm:px-0 xs:px-20 px-0">
-        {
-          
-       productData && productData.slice(0.3)?.map((item) => (
-            
+        {productData &&
+          productData.slice(0.3)?.map((item) => (
             <a
-            href={`/blogs/${item.id}`}
-           target="_blank"
-           rel="noopener noreferrer" // For security reasons
-           key={item.vendorId}
-           className=" flex justify-start sm:w-full lg:w-1/4 mx-2 my-2" style={{  flexDirection:"column" }}>
+              href={`/blogs/${item.id}`}
+              target="_blank"
+              rel="noopener noreferrer" // For security reasons
+              key={item.vendorId}
+              className=" flex justify-start sm:w-full lg:w-1/4 mx-2 my-2"
+              style={{ flexDirection: "column" }}>
               <div
-                 className="w-1/4 flex justify-start "
+                className="w-1/4 flex justify-start "
                 style={{
                   // backgroundImage: `url(${item.feature_image || "/article4.png"})`,
-                 
-                
+
                   backgroundColor: "#D9D9D9",
                 }}
-           />
-            <img
-                  alt="Placeholder"
-                  className="block h-auto w-full card_img rounded-none"
-             src={item.feature_image}
-             style={{height:"250px"}}
-                 
-                />
+              />
+              <img
+                alt="Placeholder"
+                className="block h-auto w-full card_img rounded-none"
+                src={item.feature_image}
+                style={{ height: "210px" }}
+              />
               <h3 className="font-bold lg:text-lg md:text-base text-sm text-[#000000] mt-6 leading-[21.78px]">
-              {item.Title.slice(0, 30)}
+                {item.Title.slice(0, 30)}
               </h3>
             </a>
-          //   <div
-          //   key={idx}
-          //   className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3  card_main">
-          //   <article className="overflow-hidden rounded-lg shadow-md">
-          //     <Link href="#">
-          //       <img
-          //         alt="Placeholder"
-          //         className="block h-auto w-full card_img"
-          //         src={item.feature_image}
-          //         style={{ height: "200px" }}
-          //       />
-          //     </Link>
+            //   <div
+            //   key={idx}
+            //   className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3  card_main">
+            //   <article className="overflow-hidden rounded-lg shadow-md">
+            //     <Link href="#">
+            //       <img
+            //         alt="Placeholder"
+            //         className="block h-auto w-full card_img"
+            //         src={item.feature_image}
+            //         style={{ height: "200px" }}
+            //       />
+            //     </Link>
 
-          //     <header className="flex items-center justify-between leading-tight p-2 md:p-4 ">
-          //       <h1 className="text-lg">
-          //         <Link
-          //           className="no-underline hover:underline text-black"
-          //           href="#">
-          //           {item.Title.slice(0, 15)}
-          //         </Link>
-          //       </h1>
-              
-          //     </header>
-          //     <h2 className="p-5 card_title">{item.Detail.slice(0, 40)}</h2>
-          //     <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-          //       <Link
-          //         className="flex items-center no-underline  text-black btn_read "
-                  
-          //         href={`/blogs/${item.id}`}>
-          //         <button class="rounded-lg px-4 py-2 d-flex justify-content-end text-right bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300">
-          //           Read More
-          //         </button>
-          //       </Link>
-          //     </footer>
-          //   </article>
-          // </div>
+            //     <header className="flex items-center justify-between leading-tight p-2 md:p-4 ">
+            //       <h1 className="text-lg">
+            //         <Link
+            //           className="no-underline hover:underline text-black"
+            //           href="#">
+            //           {item.Title.slice(0, 15)}
+            //         </Link>
+            //       </h1>
+
+            //     </header>
+            //     <h2 className="p-5 card_title">{item.Detail.slice(0, 40)}</h2>
+            //     <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+            //       <Link
+            //         className="flex items-center no-underline  text-black btn_read "
+
+            //         href={`/blogs/${item.id}`}>
+            //         <button class="rounded-lg px-4 py-2 d-flex justify-content-end text-right bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300">
+            //           Read More
+            //         </button>
+            //       </Link>
+            //     </footer>
+            //   </article>
+            // </div>
           ))}
       </div>
     </section>
--- components/home/NewUploads.jsx
+++ components/home/NewUploads.jsx
@@ -4,8 +4,12 @@ import React, { useEffect, useState } from "react";
 // import Link from "next/link";
 // import { apiBaseUrl } from "@/lib/constants";
 // import Loader from "@/ui/Loader";
+import Image from "next/image";
+import ReactPlayer from "react-player/lazy";
+
 import { newUploadData } from "@/app/Apicall/endPoints";
 import { SC } from "@/app/Apicall/ServerCall";
+import Link from "next/link";
 //import Carousel from "react-elastic-carousel";
 
 const NewUploads = () => {
@@ -21,38 +25,41 @@ const NewUploads = () => {
   const getFaqData = () => {
     SC.postCall(newUploadData, latestRecordCount).then((res) => {
       setData(res.data.data);
-      console.log(res.data.data);
+      console.log(res.data.data.video_Url, "new respo");
     });
   };
- 
+
   return (
     <section className="lg:pt-20 pt-16">
-             
       <h2 className="font-bold md:text-left text-center lg:text-4xl md:text-2xl text-xl text-[#000000]">
         New Uploads
       </h2>
-      
-    {data && data.length > 0 ? (
-  
-    <div className="flex gap-10 mt-10">
-      {data.map((val, idx) => {
-        console.log(val, "Data");
-        return (
-          <div className="w-auto h-auto" key={idx}>
-            {/* Your carousel content */}
-            <div className="product-des  mb-5" dangerouslySetInnerHTML={{ __html: val.video_Url }}></div>
-            <span className="font-bold text-2xl">{val.video_PresentaionName}</span>
-          </div>
-        );
-      })}
-    </div>
-  
-) : (
-  <div>
-    
-    No data available.
-  </div>
-)}
+
+      {data && data.length > 0 ? (
+        <div className="flex  gap-3 mt-10">
+          {data.map((val, idx) => {
+            console.log(val, "Data");
+            return (
+              <div className="flex flex-col sm:w-full lg:w-1/4 my-2" key={idx}>
+                <div className="mb-3">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: val && val.video_Url,
+                    }}
+                    style={{ width: "100%" }}></div>
+                </div>
+                <div>
+                  <p className="font-bold">{val.video_Description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex md:flex-nowrap flex-wrap justify-center -mx-2 mt-9 lg:mt-12">
+          <div className="p-10">No records found.</div>
+        </div>
+      )}
     </section>
   );
 };
